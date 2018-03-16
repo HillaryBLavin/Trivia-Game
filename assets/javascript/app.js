75942fe300da9678fcf68567710fac579d67b665
Hillary Lavin <hillarylavin@Hillarys-MacBook-Pro.local>
2018-03-16 10:42:53 -0400

    Add comments to game object properties, code countdown and loadQuestion

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,18 +6,36 @@ $('#start').on('click', function() {
 // Create an array of multiple-choice questions with associated images
 var questions = [{}];
 
-// Create Game Object including all game methods
+// Create Game Object including all game properties and methods
 var game = {
-    questsions: questions,
+    // Pulls questions from questions array
+    questions: questions,
+    // Indicates index of current question from array
     currentQuestion: 0,
+    // Counter (countdown for questions) starts at 20 seconds
     counter: 20,
+    // Counter for correct answers
     correct: 0,
+    // Counter for wrong answers
     wrong: 0,
+    // Create method to run countdown (counter) 
     countdown: function() {
-
+        // Decrement counter
+        game.counter --; 
+        // Display counter
+        $('#counter').html(game.counter);
+        // When time runs out, run the timeUp method
+        if(game.counter <= 0){
+            console.log("TIMES UP!");
+            game.timeUp();
+        }
     },
+    // Create method to load question to the page
     loadQuestion: function() {
-
+        // Create timer - set countdown to decrement every second
+        timer = setInterval(game.countdown, 1000);
+        // Display current question
+        $('#subwrapper').html('<h2>' + question[game.currentQuestion].question + '</h2>');
     },
     nextQuestion: function() {
 
