4dce1486f107fdbd9de77d0678f7500ed3ef20b9
Hillary Lavin <hillarylavin@Hillarys-MacBook-Pro.local>
2018-03-16 10:10:38 -0400

    Add empty array for questions, create game object scaffolding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,44 @@
+// Add on-click function that removes Start button 
 $('#start').on('click', function() {
     $('#start').remove();
 })
 
+// Create an array of multiple-choice questions with associated images
+var questions = [{}];
 
+// Create Game Object including all game methods
+var game = {
+    questsions: questions,
+    currentQuestion: 0,
+    counter: 20,
+    correct: 0,
+    wrong: 0,
+    countdown: function() {
+
+    },
+    loadQuestion: function() {
+
+    },
+    nextQuestion: function() {
+
+    },
+    timeUp: function() {
+
+    },
+    results: function () {
+
+    },
+    clicked: function () {
+
+    },
+    answeredCorrectly: function () {
+
+    },
+    answeredWrong: function () {
+
+    },
+    reset: function() {
+
+    }
+}
 
